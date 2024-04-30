1accb6a08ed84a2bbcbd6ba2621a3eeaee8a1653
bno4 <bnduchemin@gmail.com>
2024-04-30 11:28:27 +0200

    updt:grid popular.scss

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -5,8 +5,8 @@ const Item = (props) => {
       <img src={props.image} alt="item" />
       <p>{props.name}</p>
       <div className="item__prices">
-        <div className="item__prices__new">{props.new_price}</div>
-        <div className="item__prices__old">{props.old_price}</div>
+        <div className="item__prices__new">{props.new_price} €</div>
+        <div className="item__prices__old">{props.old_price} €</div>
       </div>
     </div>
   );
